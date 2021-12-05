ca72916fefc0bfcc89b7f7569ead0cd3938a3f57
Daniel Edlich <48770657+DanielEdlich@users.noreply.github.com>
2021-12-05 21:32:45 +0100

    Check number of axes.

--- src/main/java/de/htwberlin/mauterhebung/MauterServiceImpl.java
+++ src/main/java/de/htwberlin/mauterhebung/MauterServiceImpl.java
@@ -34,13 +34,45 @@ public class MauterServiceImpl implements IMauterhebung {
 		return connection;
 	}
 
+	/**
+	 * Die Methode realisiert einen Algorithmus, der die �bermittelten
+	 * Fahrzeugdaten mit der Datenbank auf Richtigkeit �berpr�ft und f�r einen
+	 * mautpflichtigen Streckenabschnitt die zu zahlende Maut f�r ein Fahrzeug
+	 * im Automatischen Verfahren berechnet.
+	 *
+	 * Zuvor wird �berpr�ft, ob das Fahrzeug registriert ist und �ber ein
+	 * eingebautes Fahrzeugger�t verf�gt und die �bermittelten Daten des
+	 * Kontrollsystems korrekt sind. Bei Fahrzeugen im Manuellen Verfahren wird
+	 * dar�berhinaus gepr�ft, ob es noch offene Buchungen f�r den Mautabschnitt
+	 * gibt oder eine Doppelbefahrung aufgetreten ist. Besteht noch eine offene
+	 * Buchung f�r den Mautabschnitt, so wird diese Buchung f�r das Fahrzeug auf
+	 * abgeschlossen gesetzt.
+	 *
+	 * Sind die Daten des Fahrzeugs im Automatischen Verfahren korrekt, wird
+	 * anhand der Mautkategorie (die sich aus der Achszahl und der
+	 * Schadstoffklasse des Fahrzeugs zusammensetzt) und der Mautabschnittsl�nge
+	 * die zu zahlende Maut berechnet, in der Mauterhebung gespeichert und
+	 * letztendlich zur�ckgegeben.
+	 */
 	@Override
 	public float berechneMaut(int mautAbschnitt, int achszahl, String kennzeichen)
 			throws UnkownVehicleException, InvalidVehicleDataException, AlreadyCruisedException {
 		if(isFahrzeugImAutomatikverfahren(kennzeichen)){
+			if ((achszahl < 5 && getAutomaticAchszahl(kennzeichen) == achszahl) || (achszahl >= 5 && getAutomaticAchszahl(kennzeichen) >= 5)){
+
+			}
+			else {
+				throw new InvalidVehicleDataException();
+			}
 
 		}
 		else if (isFahrzeugImManuellenVerfahren(kennzeichen)){
+			if ((achszahl < 5 && getAutomaticAchszahl(kennzeichen) == achszahl) || (achszahl >= 5 && getAutomaticAchszahl(kennzeichen) >= 5)){
+
+			}
+			else {
+				throw new InvalidVehicleDataException();
+			}
 
 		}
 		else {
@@ -66,8 +98,10 @@ public class MauterServiceImpl implements IMauterhebung {
 						return true;
 					}
 				}
-				else
-					throw new UnkownVehicleException();
+				else {
+					return false;
+				}
+//					throw new UnkownVehicleException();
 			}
 		} catch (SQLException e) {
 			L.error("", e);
@@ -84,8 +118,12 @@ public class MauterServiceImpl implements IMauterhebung {
 
 		try (Statement statement = getConnection().createStatement()) {
 			try (ResultSet resultSet = statement.executeQuery(sql)) {
-				if(resultSet.next()) return true;
-					else return false;
+				if(resultSet.next()) {
+					return true;
+				}
+				else {
+					return false;
+				}
 			}
 		} catch (SQLException e) {
 			L.error("", e);
@@ -93,7 +131,54 @@ public class MauterServiceImpl implements IMauterhebung {
 		}
 	}
 
+	private int getAutomaticAchszahl(String kennzeichen){
+		var sql = "select MK.ACHSZAHL from FAHRZEUG F " +
+				"join FAHRZEUGGERAT FG on F.FZ_ID = FG.FZ_ID " +
+				"join MAUTERHEBUNG M on FG.FZG_ID = M.FZG_ID " +
+				"join MAUTKATEGORIE MK on M.KATEGORIE_ID = MK. KATEGORIE_ID " +
+				"where F.KENNZEICHEN = '" + kennzeichen + "'";
+		L.info(sql);
 
+		try (Statement statement = getConnection().createStatement()){
+			try (ResultSet resultSet = statement.executeQuery(sql)) {
+				if (resultSet.next()) {
+					String achszhl = resultSet.getString(1);
+
+					//var a = Integer.parseInt(String.valueOf(achszhl.charAt(achszhl.length()-1)));
+					return Integer.parseInt(String.valueOf(achszhl.charAt(achszhl.length()-1)));
+
+				} else {
+					throw new UnkownVehicleException();
+				}
+			}
+		} catch (SQLException e) {
+			L.error("", e);
+			throw new DataException();
+		}
+	}
+
+	private int getManualAchszahl(String kennzeichen){
+		var sql = "select MK.ACHSZAHL from BUCHUNG B " +
+				"join MAUTKATEGORIE MK on B.KATEGORIE_ID = MK. KATEGORIE_ID " +
+				"where B.KENNZEICHEN = '" + kennzeichen + "' and B.B_ID = 1";
+		L.info(sql);
+
+		try (Statement statement = getConnection().createStatement()){
+			try (ResultSet resultSet = statement.executeQuery(sql)) {
+				if (resultSet.next()) {
+					String achszhl = resultSet.getString(1);
+
+					return Integer.parseInt(String.valueOf(achszhl.charAt(achszhl.length()-1)));
+
+				} else {
+					throw new UnkownVehicleException();
+				}
+			}
+		} catch (SQLException e) {
+			L.error("", e);
+			throw new DataException();
+		}
+	}
 
 
 
