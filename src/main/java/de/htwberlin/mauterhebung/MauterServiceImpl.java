8660c7d179d25e63b5f9bdc9e8f0f57ea62914ab
Daniel Edlich <48770657+DanielEdlich@users.noreply.github.com>
2021-12-05 18:07:22 +0100

    Check kind of process.

--- src/main/java/de/htwberlin/mauterhebung/MauterServiceImpl.java
+++ src/main/java/de/htwberlin/mauterhebung/MauterServiceImpl.java
@@ -37,9 +37,64 @@ public class MauterServiceImpl implements IMauterhebung {
 	@Override
 	public float berechneMaut(int mautAbschnitt, int achszahl, String kennzeichen)
 			throws UnkownVehicleException, InvalidVehicleDataException, AlreadyCruisedException {
+		if(isFahrzeugImAutomatikverfahren(kennzeichen)){
+
+		}
+		else if (isFahrzeugImManuellenVerfahren(kennzeichen)){
+
+		}
+		else {
+			throw new UnkownVehicleException();
+		}
+
+
+
+
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
+	private boolean isFahrzeugImAutomatikverfahren(String kennzeichen){
+		var sql = "select ABMELDEDATUM, STATUS from FAHRZEUG F join FAHRZEUGGERAT FG on F.FZ_ID = FG.FZ_ID where F.KENNZEICHEN = '"
+			+ kennzeichen + "'";
+		L.info(sql);
+
+		try (Statement statement = getConnection().createStatement()){
+			try (ResultSet resultSet = statement.executeQuery(sql)){
+				if(resultSet.next()){
+					if (resultSet.getString("STATUS").equals("active") && resultSet.getObject("ABMELDEDATUM") == null){
+						return true;
+					}
+				}
+				else
+					throw new UnkownVehicleException();
+			}
+		} catch (SQLException e) {
+			L.error("", e);
+			throw new DataException();
+		}
+
+		return false;
+	}
+
+	private boolean isFahrzeugImManuellenVerfahren(String kennzeichen){
+		var sql = "select * from BUCHUNG where B_ID = 1 AND KENNZEICHEN = '" + kennzeichen + "'";
+
+		L.info(sql);
+
+		try (Statement statement = getConnection().createStatement()) {
+			try (ResultSet resultSet = statement.executeQuery(sql)) {
+				if(resultSet.next()) return true;
+					else return false;
+			}
+		} catch (SQLException e) {
+			L.error("", e);
+			throw new DataException();
+		}
+	}
+
+
+
 
 
 
