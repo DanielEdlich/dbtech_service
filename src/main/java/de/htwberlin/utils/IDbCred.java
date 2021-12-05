4a0a40bcac8bb4166d699f35945a7a7ae791c5cd
Daniel Edlich <48770657+DanielEdlich@users.noreply.github.com>
2021-12-05 14:53:47 +0100

    Initial commit. Includes provided code.

---
+++ src/main/java/de/htwberlin/utils/IDbCred.java
@@ -0,0 +1,10 @@
+package de.htwberlin.utils;
+
+public interface IDbCred {
+    String driverClass = "oracle.jdbc.driver.OracleDriver";
+    String url = "jdbc:oracle:thin:@xxx";
+    String user = "uxxx";
+    String password = "pxxx";
+    String schema = "uxxx";
+}
+
